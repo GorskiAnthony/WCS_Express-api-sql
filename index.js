43b0f4582ac0589cd73d815fa4fb7d95bc12d425
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-04-25 15:59:17 +0200

    init project

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("coucou");
+/**
+ * Utilisation du fichier .env
+ */
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// Si la variable d'environnement PORT n'existe pas, alors prend le port 5050
+const PORT = process.env.PORT || 5050;
+
+/**
+ * Ne pas toucher les lignes du dessous ⬇️
+ */
+app.listen(PORT, (err) => {
+	if (err) {
+		console.error("Oups, une erreur s'est produite");
+	} else {
+		console.log(`L'application tourne sur le port: ${PORT}`);
+	}
+});
