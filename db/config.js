70b4d966c82546b709c60f60074526f99e669c9c
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-05-09 15:21:22 +0200

    update db

--- db/config.js
+++ db/config.js
@@ -11,4 +11,12 @@ const connection = mysql.createConnection({
 	database: process.env.DB_DATABASE,
 });
 
+connection.connect((err) => {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id: " + connection.threadId);
+});
+
 module.exports = connection.promise();
